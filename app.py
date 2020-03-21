1f2c4a44160f3eaac2f366bcfcd03d7fee1c8530
zuzan <urbanova.zuzana.rv@gmail.com>
2020-03-21 21:17:45 +0100

    basic upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 
 
 from flask import Flask
-from flask import render_template, send_from_directory
+from flask import render_template, send_from_directory,request
 app = Flask(__name__)
 
 
@@ -17,6 +17,20 @@ def hello():
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory('static', path)
+
+@app.route("/upload-image", methods=["GET", "POST"])
+def upload_image():
+
+    if request.method == "POST":
+
+        if request.files:
+
+            image = request.files["image"]
+
+            print(image)
+
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run()
