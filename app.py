7a337afebb573a3d5283466985553ad280e4fb2d
zuzan <urbanova.zuzana.rv@gmail.com>
2020-03-21 21:58:20 +0100

    dispaly file path + accept only xlsx + changed icon

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-
-
 #set FLASK_ENV=development
 
-
+import os
 from flask import Flask
-from flask import render_template, send_from_directory,request
+from flask import render_template, send_from_directory,request, redirect
 app = Flask(__name__)
 
+app.config["OCpR_file_UPLOADS"] = "C:\\Users\\zuzan\\Desktop\\Projects\\steal\\uploads"
+app.config["ALLOWED_OCpR_file_EXTENSIONS"] = ["XLX"]
+
+def allowed_image(filename):
+
+    if not "." in filename:
+        return False
+
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["ALLOWED_OCpR_file_EXTENSIONS"]:
+        return True
+    else:
+        return False
 
 @app.route('/')
 @app.route('/index')
@@ -18,19 +30,27 @@ def hello():
 def send_js(path):
     return send_from_directory('static', path)
 
-@app.route("/upload-image", methods=["GET", "POST"])
-def upload_image():
+@app.route("/upload-OCpR_file", methods=["GET", "POST"])
+def upload_OCpR_file():
 
     if request.method == "POST":
 
         if request.files:
 
-            image = request.files["image"]
+            OCpR_file = request.files["OCpR_file"]
 
-            print(image)
+            if OCpR_file.filename == "":
+                print("No filename")
+                return redirect("index")
 
-    return render_template("index.html")
+            if allowed_image(OCpR_file.filename):
+                OCpR_file.save(os.path.join(app.config["OCpR_file_UPLOADS"], OCpR_file.filename))
 
+                print("OCpR_file saved")
+
+    return redirect("index")
+
+    
 
 if __name__ == '__main__':
     app.run()
